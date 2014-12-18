dc0804378480e159bcf23f0673a6a06f8c223bfc
glennmschultz <glennmschultz@me.com>
2014-12-17 20:11:36 -0600

    Bond Lab Accrual bond waterfall validated

---
+++ sandbox/REMICPACZ.R
@@ -0,0 +1,145 @@
+# ==== The code below creates the deal data structures for BondLabSMBS ==
+
+
+MakeSchedule(bond.id = "BondLabMBS4",
+                DealName = "BondLabPACZ",
+                Group = 1,
+                original.bal = 200000000,
+                first.pmtdate = "01-25-2013",
+                trade.date = "01-10-2013",
+                settlement.date = "01-13-2013",
+                price = 105.75,
+                begin.cpr = .2,
+                end.cpr = 6,
+                seasoning.period = 30,
+                lower.PSA = 75, 
+                upper.PSA = 250)
+
+MakeRAID(DealName = "BondLabPACZ", 
+             Issuer = "Bondlab", 
+             DealPriceDate = "12-01-2012", 
+             DealSettlementDate = "01-01-2013",
+             Underwriter = "Bondlab",
+             NumberofTranches = 3,
+             NumberPacSchedules = 1,
+             NumberofGroups = 1,
+             DealSize = 200000000,
+             CollateralAmount = 200000000)
+      
+    MakeCollateral(DealName = "BondLabPACZ",
+                   Group = 1,
+                   Cusip = list("bondlabMBS4"),
+                   OrigBal = list("200000000"))
+     
+    MakeTranche(DealName = "BondLabPACZ",
+                TrancheNumber = "1",
+                TrancheName = "A",
+                TranchePrincipal = "Pass_Through",
+                TrancheInterest = "Fix",
+                TranchePrincipalDesc = "PAC",
+                TrancheInterestDesc = "Fix",
+                Cusip = "BondLabPACZ",
+                TrancheOrigBal = 148769215,
+                TrancheDatedDate  = "01-01-2013",
+                TrancheFirstPmtDate = "01-15-2013",
+                TrancheLastPmtDate = "12-15-2042",
+                TrancheNextPmtDate = "01-15-2013",
+                TrancheCoupon = 4.0,
+                Delay = 15,
+                PrinPmtFrequency = 12,
+                InterestPmtFrequency = 12,
+                FloaterIndex = "999",
+                FloaterMargin = 0,
+                FloaterCap = 0,
+                FloaterFloor = 0,
+                FloaterFormula = function(){0},
+                PacLowBand = 75,
+                PacHighBand = 250,
+                Group = 1,
+                Schedule = TRUE,
+                Fixed = TRUE)
+      
+      MakeTranche(DealName = "BondLabPACZ",
+                  TrancheNumber = "2",
+                  TrancheName = "B",
+                  TranchePrincipal = "Pass_Through",
+                  TrancheInterest = "Fix",
+                  TranchePrincipalDesc = "CMP",
+                  TrancheInterestDesc = "Fix",
+                  Cusip = "BondLabCMP_Z",
+                  TrancheOrigBal = 36230785,
+                  TrancheDatedDate  = "01-01-2013",
+                  TrancheFirstPmtDate = "01-15-2013",
+                  TrancheLastPmtDate = "12-15-2042",
+                  TrancheNextPmtDate = "01-15-2013",
+                  TrancheCoupon = 4.00,
+                  Delay = 15,
+                  PrinPmtFrequency = 12,
+                  InterestPmtFrequency = 12,
+                  FloaterIndex = "999",
+                  FloaterMargin = 0,
+                  FloaterCap = 0,
+                  FloaterFloor = 0,
+                  FloaterFormula = function(){0},
+                  PacLowBand = 000,
+                  PacHighBand = 000,
+                  Group = 1,
+                  Schedule = TRUE,
+                  Fixed = TRUE)
+
+        MakeTranche(DealName = "BondLabPACZ",
+                  TrancheNumber = "3",
+                  TrancheName = "C",
+                  TranchePrincipal = "Pass_Through",
+                  TrancheInterest = "Fix",
+                  TranchePrincipalDesc = "Accrual",
+                  TrancheInterestDesc = "Fix",
+                  Cusip = "BondLabZ",
+                  TrancheOrigBal = 15000000,
+                  TrancheDatedDate  = "01-01-2013",
+                  TrancheFirstPmtDate = "01-15-2013",
+                  TrancheLastPmtDate = "12-15-2042",
+                  TrancheNextPmtDate = "01-15-2013",
+                  TrancheCoupon = 4.00,
+                  Delay = 15,
+                  PrinPmtFrequency = 12,
+                  InterestPmtFrequency = 12,
+                  FloaterIndex = "999",
+                  FloaterMargin = 0,
+                  FloaterCap = 0,
+                  FloaterFloor = 0,
+                  FloaterFormula = function(){0},
+                  PacLowBand = 000,
+                  PacHighBand = 000,
+                  Group = 1,
+                  Schedule = TRUE,
+                  Fixed = TRUE)
+  
+      MakeRDME(DealName = "BondLabPACZ",
+               TrancheNumber = 1,
+               Cusip = "BondLabPAC_Z",
+               PaymentDate = "01-01-2013",
+               Coupon = 4.0,
+               Factor = 1)
+      
+      MakeRDME(DealName = "BondLabPACZ",
+               TrancheNumber = 2,
+               Cusip = "BondLabCMP_Z",
+               PaymentDate = "1-01-2013",
+               Coupon = 4.0,
+               Factor = 1)
+
+      MakeRDME(DealName = "BondLabPACZ",
+         TrancheNumber = 3,
+         Cusip = "BondLabZ",
+         PaymentDate = "1-01-2013",
+         Coupon = 4.0,
+         Factor = 1)
+
+
+  
+    RemicStructure("BondLabPACZ")   
+    
+    REMIC <- readRDS("~/BondLab/REMICData/BondLabPACZ.rds")
+
+   

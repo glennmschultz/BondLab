8669ad6f7e96eeed04f6e7e9837bd64cc540df41
Glenn Schultz <glennmschultz@me.com>
2015-08-23 20:32:17 -0500

    connection saveMBS

--- R/PassThroughConstructor.R
+++ R/PassThroughConstructor.R
@@ -350,11 +350,11 @@ setMethod("initialize",
     Burnout = Burnout,
     SATO = SATO)
   
- # connMBSDetails <- gzfile(description = paste(system.file(package = "BondLab"),
- #                                      "/BondData/",temp@ID,".rds", sep = ""))
- #  saveRDS(temp, connMBSDetails)
- #  close(connMBSDetails)
-  SaveMBS(filename = "temp")
+   connMBSDetails <- gzfile(description = paste(system.file(package = "BondLab"),
+                                       "/BondData/",temp@ID,".rds", sep = ""))
+   saveRDS(temp, connMBSDetails)
+   close(connMBSDetails)
+ #  SaveMBS(filename = "temp")
 }
 
 setGeneric("MakeMBSDetails", function(

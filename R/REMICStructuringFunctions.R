7ea522a896d09e40c40164723764f26255872614
Glenn Schultz <glennmschultz@me.com>
2016-08-23 15:42:15 -0500

    deleted file cashflow_array

--- R/REMICStructuringFunctions.R
+++ R/REMICStructuringFunctions.R
@@ -35,16 +35,49 @@
       settlement.date = settlement.date,
       principal = principal,
       PrepaymentAssumption = Vector)}
+  
+  #' Function to assign the beginning principal balance to REMIC tranche
+  #'
+  #' This function takes the ending balance of the previous period and assigns
+  #' the value to the beginning balance in the current period. Structuring functions
+  #' run on a tree structure created by the package data.tree.  See data.tree for more details on the package.
+  #' @param node The entry point of the deal's tree structure.
+  #' @param period A numeric value the period in which payment is recieved stated as an integer.  
+  BeginBal <- function(node, period = numeric()){
+    if(period == 1) {node$BeginBal[period] <- node$CurrBal
+    } else {node$BeginBal[period] <- node$EndingBal[period-1]}}
+  
+  #' Function to calculate interest in the current period based on Beginning Balance
+  #' 
+  #' The function takes beginning balance and frequency and period to calculate interest
+  #' due the investor in the period.  The coupon is passed via the node argument from the
+  #' deal's tree structure.  Structuring functions run on a tree structure created by the 
+  #' package data.tree.  See data.tree for more details on the package.
+  #' @param node The entry point of the deal's tree structure
+  #' @param frequency the number of payments received over the calendar year
+  #' @param period A numeric value the period in which payment is received stated as integer
+  Interest <- function(node, frequency, period = numeric()){
+    node$Interest = node$Coupon/(yield.basis * frequency) * node$BeginBal}
+  
+  #' Function to assign the ending principal balance to REMIC tranche
+  #' 
+  #' This function takes the beginning balance of the current period to calculate the 
+  #' ending balance of the current period based on the principal paid in the period.  The
+  #' principal paid in the period is passed via the node$Principal argument 
+  #' Structuring functions run on a tree structure created by the package data.tree
+  #' @param node The entry point of the deal's tree structure
+  #' @param period A numeric value the period in which payment is received stated as integer
+  EndingBal <- function(node, period = numeric()){
+    node$EndingBal[period] = as.numeric(node$BeginBal[period]) - as.numeric(node$Principal[period])}
 
-    
-    #' A function to calculate the floater coupon 
-    #' 
-    #' The function calculates the floater coupon given
-    #' @param cap a numeric value the floater cap
-    #' @param floor a numeric value the floater floor
-    #' @param margin a numeric value the floater margin
-    #' @param index a numeric value the index value
-    #' @param multiplier a numeric value the index multiplier
-    #' @export FloatCoupon
-    FloatCoupon <- function(cap, floor, margin, index, multiplier){
-      min(cap, max((index * multiplier) + margin, floor))}
+  #' A function to calculate the floater coupon 
+  #' 
+  #' The function calculates the floater coupon given
+  #' @param cap a numeric value the floater cap
+  #' @param floor a numeric value the floater floor
+  #' @param margin a numeric value the floater margin
+  #' @param index a numeric value the index value
+  #' @param multiplier a numeric value the index multiplier
+  #' @export FloatCoupon
+  FloatCoupon <- function(cap, floor, margin, index, multiplier){
+    min(cap, max((index * multiplier) + margin, floor))}

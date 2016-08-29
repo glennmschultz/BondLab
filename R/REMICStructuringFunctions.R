10699c652142d92e8ee15256a868104520a663e9
Glenn Schultz <glennmschultz@me.com>
2016-08-29 15:18:00 -0500

    REMIC structuring

--- R/REMICStructuringFunctions.R
+++ R/REMICStructuringFunctions.R
@@ -1,7 +1,9 @@
 
   #' A function to calcualte the cashflow of the REMIC collateral
   #' 
-  #' Calculate the REMIC collateral cashflow
+  #' Calculate the REMIC collateral cashflow.  This function is used to
+  #' structure a REMIC at the pricing speed.  This function is also used to 
+  #' reverse engineer a REMIC
   #' @param bond.id a character the bond cusip or id
   #' @param settlement.date a character the settlement date
   #' @param term.structure a character refering to object of type TermStructure
@@ -39,36 +41,44 @@
   #' Function to assign the beginning principal balance to REMIC tranche
   #'
   #' This function takes the ending balance of the previous period and assigns
-  #' the value to the beginning balance in the current period. Structuring functions
-  #' run on a tree structure created by the package data.tree.  See data.tree for more details on the package.
+  #' the value to the beginning balance in the current period. Structuring 
+  #' functions run on a tree structure created by the package data.tree.  
+  #' See data.tree for more details on the package.
   #' @param node The entry point of the deal's tree structure.
-  #' @param period A numeric value the period in which payment is recieved stated as an integer.  
+  #' @param period A numeric value the period in which payment is recieved 
+  #' stated as an integer.  
   BeginBal <- function(node, period = numeric()){
     if(period == 1) {node$BeginBal[period] <- node$CurrBal
     } else {node$BeginBal[period] <- node$EndingBal[period-1]}}
   
-  #' Function to calculate interest in the current period based on Beginning Balance
+  #' Function to calculate interest in the current period based on Beginning 
+  #' Balance.
   #' 
-  #' The function takes beginning balance and frequency and period to calculate interest
-  #' due the investor in the period.  The coupon is passed via the node argument from the
-  #' deal's tree structure.  Structuring functions run on a tree structure created by the 
+  #' The function takes beginning balance and frequency and period to 
+  #' calculate interest due the investor in the period.  The coupon is passed 
+  #' via the node argument from the deal's tree structure.  Structuring 
+  #' functions run on a tree structure created by the 
   #' package data.tree.  See data.tree for more details on the package.
   #' @param node The entry point of the deal's tree structure
   #' @param frequency the number of payments received over the calendar year
-  #' @param period A numeric value the period in which payment is received stated as integer
+  #' @param period A numeric value the period in which payment is received 
+  #' stated as integer
   Interest <- function(node, frequency, period = numeric()){
     node$Interest = node$Coupon/(yield.basis * frequency) * node$BeginBal}
   
   #' Function to assign the ending principal balance to REMIC tranche
   #' 
-  #' This function takes the beginning balance of the current period to calculate the 
-  #' ending balance of the current period based on the principal paid in the period.  The
-  #' principal paid in the period is passed via the node$Principal argument 
-  #' Structuring functions run on a tree structure created by the package data.tree
+  #' This function takes the beginning balance of the current period to 
+  #' calculate the ending balance of the current period based on the principal 
+  #' paid in the period.  The principal paid in the period is passed via the 
+  #' node$Principal argument Structuring functions run on a tree structure 
+  #' created by the package data.tree
   #' @param node The entry point of the deal's tree structure
-  #' @param period A numeric value the period in which payment is received stated as integer
+  #' @param period A numeric value the period in which payment is received 
+  #' stated as integer
   EndingBal <- function(node, period = numeric()){
-    node$EndingBal[period] = as.numeric(node$BeginBal[period]) - as.numeric(node$Principal[period])}
+    node$EndingBal[period] = as.numeric(node$BeginBal[period]) - 
+      as.numeric(node$Principal[period])}
 
   #' A function to calculate the floater coupon 
   #' 
--- inst/REMICData/2016_053.R
+++ inst/REMICData/2016_053.R
@@ -6,28 +6,9 @@
                     "/REMICData/2016_053", sep ="")
   source(REMICConn, local = TRUE, echo =FALSE)
   
-  SCHEDConn <- paste(system.file(package="BondLab"),
-                     "/REMICData/2016_053_Schedules.txt", sep ="")
+  REMICConn <- "/Users/glennschultz/Library/R/3.3/library/BondLab/REMICData/2016_053" 
+  source(REMICConn)
   
-  schedule <- readLines(SCHEDConn)
-  schedule <- schedule[c(seq(-8, -1, 1))]
-  schedule <- sub(",", "", schedule)
-  schedule <- gsub("\\.{12}","", schedule)
-  schedule <- gsub("\\.{11}","", schedule)
-  schedule <- gsub("\\.{10}","", schedule)
-  schedule <- gsub("\\.{9}","", schedule)
-  schedule <- gsub("\\.{8}","", schedule)
-  schedule <- gsub("\\.{7}","", schedule)
-  schedule <- gsub("\\.{6}","", schedule)
-  schedule <- gsub("\\.{5}","", schedule)
-  schedule <- gsub("\\.{4}","", schedule)
-  schedule <- gsub("\\.{3}","", schedule)
-  schedule <- gsub("\\.{2}","", schedule)
-  schedule <- gsub("^[:space:] ", ",", schedule)
- 
-  SCHEDG1Conn <- paste(system.file(package="BondLab"),
-                     "/REMICData/2016_053_Schedules_G1.txt", sep ="")
- write.table(schedule, file = SCHEDG1Conn, row.names = FALSE, sep = " ")
- 
- Schedules<- read.csv(SCHEDG1Conn,row.names = NULL)
+  
+  
   

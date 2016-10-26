b3137adbcdde09f3037bfbc82b326b8d3bd3fe82
Glenn Schultz <glennschultz@Glenns-MacBook-Pro.local>
2016-10-26 10:33:27 -0500

    rgl.useNLL = TRUE set in options for Travis

--- R/BondLab.R
+++ R/BondLab.R
@@ -18,6 +18,7 @@
   # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
   options(digits = 8)
+  options(rgl.useNULL = TRUE) 
   utils::globalVariables(c("day", "month", "year"))
 
 

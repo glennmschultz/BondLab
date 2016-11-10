a2c393de765d58323ad13110262ad8391e0ac703
Glenn Schultz <glennschultz@Glenns-MacBook-Pro.local>
2016-11-10 09:18:50 -0600

    build vignette

--- vignettes/BondLab.R
+++
@@ -1,8 +0,0 @@
-## ---- setup, include=FALSE-----------------------------------------------
-knitr::opts_chunk$set(cache=TRUE)
-require(ggplot2)
-require(reshape2)
-require(scales)
-require(termstrc)
-require(BondLab)
-

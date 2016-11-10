8e6351a94d15a7d92abdfebc7c52466496225c39
Glenn Schultz <glennschultz@Glenns-MacBook-Pro.local>
2016-11-10 09:31:57 -0600

    removed inst/doc from gitignore

---
+++ vignettes/BondLab.R
@@ -0,0 +1,8 @@
+## ---- setup, include=FALSE-----------------------------------------------
+knitr::opts_chunk$set(cache=TRUE)
+require(ggplot2)
+require(reshape2)
+require(scales)
+require(termstrc)
+require(BondLab)
+

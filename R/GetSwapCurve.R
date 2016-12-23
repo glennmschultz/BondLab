b575d5ffd6e208205b2348f3eadefd86ff45dfdb
Glenn Schultz <glennschultz@Glenns-MacBook-Pro.local>
2016-12-22 21:08:36 -0600

    commit swap curve from ice

--- R/GetSwapCurve.R
+++ R/GetSwapCurve.R
@@ -22,67 +22,69 @@
   #' (FRED) database.  Full swap curve data is reported with a lag of 
   #' approximately four days due to delayed reporting of LIBOR rates by ICE
   #' @importFrom RCurl getURL
-  #' @importFrom lubridate days
-  #' @importFrom XML xmlParse 
-  #' @importFrom XML xmlGetAttr
-  #' @importFrom XML xpathSApply
+  #' @importFrom httr GET
+  #' @importFrom httr set_cookies
+  #' @importFrom httr write_disk
   #' @param CurveDate a character the swap curve date
-  #' @param APIKey a character the user API Key provided by FRED
   #' @export GetSwapCurve
-  GetSwapCurve <- function(CurveDate = "character",
-                         APIKey = "character"){
-  rates <- list("USD1MTD156N", 
-                "USD3MTD156N", 
-                "USD6MTD156N",
-                "USD12MD156N",
-                "DSWP2", 
-                "DSWP3", 
-                "DSWP4", 
-                "DSWP5",
-                "DSWP7", 
-                "DSWP10", 
-                "DSWP30")
-  
-  SwapData <- matrix(data = NA, nrow = 1, ncol = 12)
-  colnames(SwapData) <- c("Date", 
-                          "ED1M", 
-                          "ED3M", 
-                          "ED6M", 
-                          "USSW1", 
-                          "USSW2", 
-                          "USSW3", 
-                          "USSW4", 
-                          "USSW5", 
-                          "USSW7", 
-                          "USSW10", 
-                          "USSW30")
-  
-  startdate <- as.character(as.Date(
-    as.character(CurveDate), format = "%m-%d-%Y") + days(0))
-  enddate <- as.character(
-    as.Date(as.character(CurveDate), format = "%m-%d-%Y"))
-  SwapData[1,1] = startdate 
-  
-  for(i in 1:11){
+  GetSwapCurve <- function(CurveDate = "character"){
+  dataSwapURL <- "https://www.theice.com/marketdata/reports/icebenchmarkadmin/ISDAFIXHistoricalRates.shtml"
+    dataLiborURL <- "https://www.theice.com/marketdata/reports/icebenchmarkadmin/ICELiborHistoricalRates.shtml"
+    SwapPath <- paste(system.file(package = "BondLab"),"/RatesData/Swap.csv", sep = "")
+    LiborPath <- paste(system.file(package = "BondLab"),"/RatesData/Libor.csv", sep = "")
+    dataRes <- GET(dataSwapURL,
+                   query = list(excelExport = "",
+                                criteria.reportDate = as.character(CurveDate),
+                                criteria.seriesName = I("USD+Rates"),
+                                criteria.runCode = "1100"),
+                   set_cookies(iceBanner = "rcDisclaimer"), 
+                   write_disk(SwapPath, overwrite = TRUE)
+    )
+    swapdata <- read.csv(SwapPath)
     
-    LIBORRate <- getURL(
-      paste("https://api.stlouisfed.org/fred/series/observations?series_id=",
-            as.character(rates[i]),
-            #"&realtime_start=",startdate, 
-            #"&realtime_end=", enddate,
-            "&observation_start=", startdate,
-            "&observation_end=", enddate, 
-            "&api_key=", APIKey,
-            "&file_type=xml", sep =""))
-    doc = xmlParse(LIBORRate)
-    RateValue <- as.numeric(
-      xpathSApply(doc, "//observation", xmlGetAttr, "value"))
-    SwapData[1,as.numeric(i) +1] = RateValue}
-  
-  write.csv(SwapData, paste(system.file(package = "BondLab"),
-                            "/RatesData/Today.csv", sep = ""),
-            row.names = FALSE)
-  
-  maturity <- c("",.0833, .25, .5, 1, 2, 3, 4, 5, 7, 10, 30)
-  try(SwapRateData(datafile = paste(system.file(package = "BondLab"),"/RatesData/Today.csv", sep = ""), 
-                   maturityvector = maturity))}
+    dataRes <- GET(dataLiborURL,
+                   query = list(excelExport = "",
+                                criteria.reportDate = as.character(CurveDate),
+                                criteria.currencyCode = "USD"),
+                   set_cookies(iceBanner = "rcDisclaimer"), 
+                   write_disk(LiborPath, overwrite = TRUE)
+    )
+    libordata <- read.csv(LiborPath)
+    
+    #Dimension SwapRateData array
+    SwapData <- matrix(data = NA, nrow = 1, ncol = 12)
+    colnames(SwapData) <- c("Date", 
+                            "ED1M", 
+                            "ED3M", 
+                            "ED6M", 
+                            "USSW1", 
+                            "USSW2", 
+                            "USSW3", 
+                            "USSW4", 
+                            "USSW5", 
+                            "USSW7", 
+                            "USSW10", 
+                            "USSW30")
+    
+    SwapData[1,1] <- as.character(as.Date(
+      as.character(CurveDate), format = "%m-%d-%Y"))
+    SwapData[1,2] <- libordata[3,3] #1 month libor rate
+    SwapData[1,3] <- libordata[5,3] #3 month libor rate
+    SwapData[1,4] <- libordata[6,3] #6 month libor rate
+    SwapData[1,5] <- libordata[7,3] #12 month libor rate 
+    SwapData[1,6] <- swapdata[2,2]  #2 year swap rate
+    SwapData[1,7] <- swapdata[3,2]  #3 year swap rate
+    SwapData[1,8] <- swapdata[4,2]  #4 year swap rate
+    SwapData[1,9] <- swapdata[5,2]  #5 year swap rate
+    SwapData[1,10] <- swapdata[7,2] #7 year swap rate
+    SwapData[1,11] <- swapdata[10,2]#10 year swap rate
+    SwapData[1,12] <- swapdata[13,2]#30 year swap rate
+    
+    write.csv(SwapData, paste(system.file(package = "BondLab"),
+                              "/RatesData/Today.csv", sep = ""),
+              row.names = FALSE)
+    
+    maturity <- c("",.0833, .25, .5, 1, 2, 3, 4, 5, 7, 10, 30)
+    try(SwapRateData(datafile = paste(system.file(package = "BondLab"),"/RatesData/Today.csv", sep = ""), 
+                     maturityvector = maturity))
+  }

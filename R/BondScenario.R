03e1cb3e0cc6d3a02bd08ced1a7e2a01abdc397d
Glenn Schultz <glennschultz@Glenns-MacBook-Pro.local>
2017-04-08 11:02:50 -0500

    final bond scenario

--- R/BondScenario.R
+++ R/BondScenario.R
@@ -328,7 +328,8 @@
     }
     
     
-    if(as.Date(LastPmtDate(HorizonBond), format = '%m-%d-%Y') <= as.Date(NextPmtDate(HorizonBond), format = '%m-%d-%Y')){
+    if(as.Date(LastPmtDate(HorizonBond), format = '%m-%d-%Y') <= 
+       as.Date(NextPmtDate(HorizonBond), format = '%m-%d-%Y')){
     HorizonBond <- `NextPmtDate<-`(HorizonBond,
                                    as.character(format(
                                      as.Date(LastPmtDate(HorizonBond), format = "%m-%d-%Y") %m+% 

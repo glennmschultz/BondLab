d855e362b5c7fd38da500faa195e6f8582b1b39a
Glenn Schultz <glennschultz@Glenns-MacBook-Pro.local>
2017-09-05 21:51:53 -0400

    total return horizon curve bug fixed

--- R/BondScenario.R
+++ R/BondScenario.R
@@ -282,8 +282,10 @@
           as.Date(rates.data[1,1]) %m+% months(horizon.months)),
         Period = numeric(),
         Date = "character",
+        TimePeriod = numeric(),
         SpotRate = numeric(),
         ForwardRate = numeric(),
+        DiscRate = numeric(),
         TwoYearFwd = numeric(),
         TenYearFwd = numeric())
       
@@ -291,8 +293,8 @@
       ForwardDate(HorizonTermStructure) <- as.character(
         as.Date(ForwardDate(TermStructure)) %m+% months(horizon.months))
       TimePeriod(HorizonTermStructure) <- TimePeriod(TermStructure)
-      ForwardRate(HorizonTermStructure) <-ForwardRate(TermStructure)
       SpotRate(HorizonTermStructure) <- SpotRate(TermStructure)
+      ForwardRate(HorizonTermStructure) <-ForwardRate(TermStructure)
       DiscRate(HorizonTermStructure) <- DiscRate(TermStructure)
       TwoYearForward(HorizonTermStructure) <- TwoYearForward(TermStructure)
       TenYearForward(HorizonTermStructure) <- TenYearForward(TermStructure)

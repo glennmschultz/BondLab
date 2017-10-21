b9240573c107bd13343f30d74464d4c3757ba3cd
Glenn Schultz <glennschultz@Glenns-MacBook-Pro.local>
2017-10-21 11:45:05 -0500

    replaced ICE swap rates with FRED UST Treasury Rates

--- R/TermStructure.R
+++ R/TermStructure.R
@@ -391,6 +391,7 @@
   #' @importFrom lubridate years
   #' @importFrom lubridate day
   #' @importFrom lubridate month
+  #' @importFrom bizdays add.bizdays
   #' @importFrom termstrc estim_nss estim_cs spotrates forwardrates
   #' @export TermStructure
   TermStructure <- function(rates.data, method = "dl"){
@@ -409,100 +410,240 @@
   CheckMethod <- c("ns", "dl", "sv", "asv", "cs")
   if(!method %in% CheckMethod) stop ("Invalid 'method' Value")
   
+  # function to compute the price of T-Bill.  This is used to price
+  # short discount securities for the Termstructure fitting
+  BillPrice <- function(face = 100, disc.rate, days.mat){
+    price = NULL
+    price = face*(1-(disc.rate * days.mat)/360)
+    return(price)}
+  
+  # create NULL CurveData object
+  CurveData <- NULL
+  # set calendar object for calculation of proper settlement date
+  cal <- 'actual'
+  
   # pass the yield curve to the function
   rates.data <- rates.data
   
+  # Dimension UST rates data array
+  USTData <- data.frame(matrix(data = NA, nrow = 2, ncol = 12))
+  colnames(USTData) <- c('Date', 
+                         'Bill1M', 
+                         'Bill3M', 
+                         'Bill6M', 
+                         'Bill1', 
+                         'Note2', 
+                         'Note3', 
+                         'Note5', 
+                         'Note7', 
+                         'Note10', 
+                         'Note20', 
+                         'Bond30')
+  
+  USTData[1,1] <- as.character(as.Date(
+    as.character(rates.data[1,1]), format = '%Y-%m-%d'))
+  USTData[1,2] <- rates.data[1,2] #1 month bill rate
+  USTData[1,3] <- rates.data[1,3] #3 month bill rate
+  USTData[1,4] <- rates.data[1,4] #6 month bill rate
+  USTData[1,5] <- rates.data[1,5] #12 month libor rate 
+  USTData[1,6] <- rates.data[1,6]  #2 year note rate
+  USTData[1,7] <- rates.data[1,7]  #3 year note rate
+  USTData[1,8] <- rates.data[1,8]  #5 year note rate
+  USTData[1,9] <- rates.data[1,9]  #7 year note rate
+  USTData[1,10] <- rates.data[1,10] #10 year note rate
+  USTData[1,11] <- rates.data[1,11] #20 year bond rate
+  USTData[1,12] <- rates.data[1,12] #30 year bond rate
+  USTData[2,2] <- .0833
+  USTData[2,3] <- .25
+  USTData[2,4] <- .50
+  USTData[2,5] <- 1
+  USTData[2,6] <- 2
+  USTData[2,7] <- 3
+  USTData[2,8] <- 5
+  USTData[2,9] <- 7
+  USTData[2,10] <- 10
+  USTData[2,11] <- 20
+  USTData[2,12] <- 30
+  
+  treasurybonds <- list(ISIN = NULL,
+                        MATURITYDATE = NULL,
+                        ISSUDATE = NULL,
+                        COUPONRATE = NULL,
+                        PRICE = NULL,
+                        ACCRUED = NULL,
+                        CASHFLOWS = list(ISIN = NULL, CF = NULL, DATE = NULL),
+                        TODAY = NULL)
+  
+  for(bonddata in 2:(length(USTData))){
+    num.months = ceiling(USTData[2,bonddata] * 12)
+    MaturityDate = as.character(as.Date(USTData[1,1]) %m+% months(num.months), format = '%m-%d-%Y')
+    SettlementDate = as.character(add.bizdays(USTData[1,1], 2, cal), format = '%m-%d-%Y')
+    IssueDate =as.character(as.Date(USTData[1,1]), format = '%m-%d-%Y')
+    DatedDate = as.character(as.Date(USTData[1,1]), format = '%m-%d-%Y')
+    Coupon = if(USTData[2,bonddata] <= 1){0.00} else {USTData[1,bonddata]}
+    Price = if(USTData[2,bonddata] <= 1) {BillPrice(
+      100, USTData[1,bonddata]/100, 
+      floor(USTData[2,bonddata] * 364))} else {100}
+    
+    if(USTData[2,bonddata] <= 1){PmtDates = c(as.Date(SettlementDate, format = '%m-%d-%Y'),
+                                              as.Date(MaturityDate, format = '%m-%d-%Y'))} else {
+                                                PmtDates = LastandNextPmtDate(
+                                                  issue.date = IssueDate,
+                                                  dated.date = DatedDate,
+                                                  maturity.date = MaturityDate,
+                                                  settlement.date = SettlementDate,
+                                                  bond.basis = 'Actual365',
+                                                  frequency = 2)}
+    
+    ISIN = gsub("[[:punct:]]", "", paste0('UST',Coupon, 
+                                          format(as.Date(MaturityDate, 
+                                                         format = '%m-%d-%Y'), 
+                                                 format ='%m%-%Y')))
+    
+    Bond <- BondDetails(
+      Cusip = ISIN,
+      ID = colnames(USTData[1]),
+      BondType = if(USTData[1,1] <=1) {'Bill'} else {'Bond'},
+      Sector = 'UST',
+      Issuer = 'US Govt',
+      Underwriter = 'US Govt',
+      OfferAmount = 100,
+      Coupon = Coupon,
+      IssueDate = IssueDate,
+      DatedDate = DatedDate,
+      Maturity = MaturityDate,
+      LastPmtDate = as.character(as.Date(PmtDates[1]), format = '%m-%d-%Y'),
+      NextPmtDate = as.character(as.Date(PmtDates[2]), format = '%m-%d-%Y'),
+      Moody = 'Aaa',
+      SP = 'AAA',
+      BondLab = 'AAA',
+      Frequency = 2,
+      BondBasis = 'Actual365',
+      Callable = FALSE,
+      Putable = FALSE,
+      SinkingFund = FALSE
+    )
+    
+    CashFlow <- CashFlowBond(bond.id = Bond, 
+                             principal = OfferAmount(Bond), 
+                             settlement.date = SettlementDate)
+    
+    treasurybonds$ISIN = append(treasurybonds$ISIN, Cusip(Bond))
+    treasurybonds$MATURITYDATE = append(treasurybonds$MATURITYDATE, Maturity(Bond))
+    treasurybonds$ISSUDATE = append(treasurybonds$ISSUDATE, IssueDate(Bond))
+    treasurybonds$COUPONRATE = append(treasurybonds$COUPONRATE, Coupon(Bond))
+    treasurybonds$PRICE = append(treasurybonds$PRICE, Price)
+    treasurybonds$ACCRUED = append(treasurybonds$ACCRUED,0)
+    treasurybonds$CASHFLOWS$ISIN = append(treasurybonds$CASHFLOWS$ISIN,
+                                          rep(Cusip(Bond), length(CashFlow[,'TotalCashFlow'])))
+    treasurybonds$CASHFLOWS$CF = append(treasurybonds$CASHFLOWS$CF,
+                                        unname(CashFlow[,'TotalCashFlow']))
+    treasurybonds$CASHFLOWS$DATE = append(treasurybonds$CASHFLOWS$DATE,
+                                          unname(as.Date(CashFlow[,'Date'], origin = '1970-01-01')))
+    treasurybonds$TODAY = USTData[1,1]
+  }
+  
+  CurveData$treasurybonds <- treasurybonds
+  class(CurveData) <- 'couponbonds'
+  
+  
+  #----------------------------------------------------------------------------
+  # this is old code that is depricated.  The code was written prior to the 
+  # bondlab bond cash flow engines and was used to cashflow swaps for term structure
+  # the new code uses the bond lab bond cashflow engine is far more understandable
   #set the column counter to make cashflows for termstrucutre
-  ColCount <- as.numeric(ncol(rates.data))
-  Mat.Years <- as.numeric(rates.data[2,2:ColCount])
-  Coupon.Rate <- as.numeric(rates.data[1,2:ColCount])
-  Issue.Date <- as.Date(rates.data[1,1])
+#  ColCount <- as.numeric(ncol(rates.data))
+#  Mat.Years <- as.numeric(rates.data[2,2:ColCount])
+#  Coupon.Rate <- as.numeric(rates.data[1,2:ColCount])
+#  Issue.Date <- as.Date(rates.data[1,1])
   
   #initialize coupon bonds S3 class
   #This can be upgraded when bondlab has portfolio function
-  ISIN <- vector()
-  MATURITYDATE <- vector()
-  ISSUEDATE <- vector()
-  COUPONRATE <- vector()
-  PRICE <- vector()
-  ACCRUED <- vector()
-  CFISIN <- vector()
-  CF <- vector()
-  DATE <- vector()
-  CASHFLOWS  <- list(CFISIN,CF,DATE)
-  names(CASHFLOWS) <- c("ISIN","CF","DATE")
-  TODAY <- vector()
-  data <- list()
-  TSInput <- list()
+#  ISIN <- vector()
+#  MATURITYDATE <- vector()
+#  ISSUEDATE <- vector()
+#  COUPONRATE <- vector()
+#  PRICE <- vector()
+#  ACCRUED <- vector()
+#  CFISIN <- vector()
+#  CF <- vector()
+#  DATE <- vector()
+#  CASHFLOWS  <- list(CFISIN,CF,DATE)
+#  names(CASHFLOWS) <- c("ISIN","CF","DATE")
+#  TODAY <- vector()
+#  data <- list()
+#  TSInput <- list()
   
   ### Assign Values to List Items #########
-  data = NULL
-  data$ISIN <- colnames(rates.data[2:ColCount])
-  data$ISSUEDATE <- rep(as.Date(rates.data[1,1]),ColCount - 1)
+#  data = NULL
+#  data$ISIN <- colnames(rates.data[2:ColCount])
+#  data$ISSUEDATE <- rep(as.Date(rates.data[1,1]),ColCount - 1)
   
-  data$MATURITYDATE <-
-    sapply(Mat.Years, function(Mat.Years = Mat.Years, 
-                               Issue = Issue.Date) {
-      Maturity = if(Mat.Years < 1) {
-        Issue %m+% months(round(Mat.Years * months.in.year))
-        } else {Issue %m+% years(as.numeric(Mat.Years))}
-    return(as.character(Maturity))
-    }) 
+#  data$MATURITYDATE <-
+#    sapply(Mat.Years, function(Mat.Years = Mat.Years, 
+#                               Issue = Issue.Date) {
+#      Maturity = if(Mat.Years < 1) {
+#        Issue %m+% months(round(Mat.Years * months.in.year))
+#        } else {Issue %m+% years(as.numeric(Mat.Years))}
+#    return(as.character(Maturity))
+#    }) 
   
   
-  data$COUPONRATE <- ifelse(Mat.Years < 1, 0, Coupon.Rate)                  
+#  data$COUPONRATE <- ifelse(Mat.Years < 1, 0, Coupon.Rate)                  
   
-  data$PRICE <- ifelse(Mat.Years < 1, 
-                       (1 + (Coupon.Rate/100))^(Mat.Years * -1) * 100,
-                       100)
+#  data$PRICE <- ifelse(Mat.Years < 1, 
+#                       (1 + (Coupon.Rate/100))^(Mat.Years * -1) * 100,
+#                       100)
   
-  data$ACCRUED <- rep(0, ColCount -1)
+#  data$ACCRUED <- rep(0, ColCount -1)
   
-  for(j in 1:(ColCount-1)){
-    Vector.Length <- as.numeric(
-      round(difftime(data[[3]][j],
-                     data[[2]][j],
-                     units = c("weeks"))/weeks.in.year,5))
+#  for(j in 1:(ColCount-1)){
+#    Vector.Length <- as.numeric(
+#      round(difftime(data[[3]][j],
+#                     data[[2]][j],
+#                     units = c("weeks"))/weeks.in.year,5))
     
-    Vector.Length <- ifelse(round(Vector.Length) < 1, 1 , 
-                            round(Vector.Length * pmt.frequency))
+#    Vector.Length <- ifelse(round(Vector.Length) < 1, 1 , 
+#                            round(Vector.Length * pmt.frequency))
     
-    data$CASHFLOWS$ISIN <- append(data$CASHFLOWS$ISIN, 
-                                  rep(data[[1]][j],Vector.Length))
+#    data$CASHFLOWS$ISIN <- append(data$CASHFLOWS$ISIN, 
+#                                  rep(data[[1]][j],Vector.Length))
     
-    data$CASHFLOWS$CF <- append(data$CASHFLOWS$CF,
-      as.numeric(c(rep((data[[4]][j]/100/pmt.frequency), Vector.Length-1) * 
-                     min.principal, 
-              (min.principal + (data$COUPONRATE[j]/100/pmt.frequency)* 
-                 min.principal))))
+#    data$CASHFLOWS$CF <- append(data$CASHFLOWS$CF,
+#      as.numeric(c(rep((data[[4]][j]/100/pmt.frequency), Vector.Length-1) * 
+#                     min.principal, 
+#              (min.principal + (data$COUPONRATE[j]/100/pmt.frequency)* 
+#                 min.principal))))
     
-    by.months = ifelse(data[[4]][j] == 0, 
-                       round(difftime(data[[3]][j], 
-                                      rates.data[1,1])/days.in.month), 6) 
+#    by.months = ifelse(data[[4]][j] == 0, 
+#                       round(difftime(data[[3]][j], 
+#                                      rates.data[1,1])/days.in.month), 6) 
   # this sets the month increment so that cashflows can handle discount bills
   
-  data$CASHFLOWS$DATE <- append(data$CASHFLOW$DATE,
-  seq(as.Date(rates.data[1,1]) %m+% months(as.numeric(by.months)), 
-  as.Date(data[[3]][j]), 
-  by = as.character(paste(by.months, "months", sep = " "))))
+#  data$CASHFLOWS$DATE <- append(data$CASHFLOW$DATE,
+#  seq(as.Date(rates.data[1,1]) %m+% months(as.numeric(by.months)), 
+#  as.Date(data[[3]][j]), 
+#  by = as.character(paste(by.months, "months", sep = " "))))
     
-  } #The Loop Ends here and the list is made
+#  } #The Loop Ends here and the list is made
   
-  data$TODAY <- as.Date(rates.data[1,1])
-  TSInput[[as.character(rates.data[1,1])]] <- c(data)
+#  data$TODAY <- as.Date(rates.data[1,1])
+#  TSInput[[as.character(rates.data[1,1])]] <- c(data)
   
   #set term strucuture input (TSInput) to class couponbonds
-  class(TSInput) <- "couponbonds"
+#  class(TSInput) <- "couponbonds"
   
-  TS <- TSInput
+#  TS <- TSInput
+  # this is end of the old code
+  # ----------------------------------------------------------------------------
   
   #Fit the term structure of interest rates
   
-  if(method != "cs") {TSFit <- estim_nss(dataset = TSInput, 
-                                        group = as.character(rates.data[1,1]), 
+  if(method != "cs") {TSFit <- estim_nss(dataset = CurveData, 
+                                        group = 'treasurybonds', 
                                         matrange = "all", method = method)
-  } else {TSFit <- estim_cs(bonddata = TSInput, 
-                     group = as.character(rates.data[1,1]), 
+  } else {TSFit <- estim_cs(bonddata = CurveData, 
+                     group = 'treasurybonds', 
                      matrange = "all", rse = TRUE)}
   
   #Return the coefficient vector to be passed in to the spot and 
@@ -535,11 +676,15 @@
   
   
   #Calculate the spot rate curve and determine the forward rates needed to 
-  period <- seq(from = 1, to = 600, by = 1)
+  #period <- seq(from = 1, to = 600, by = 1)
   
   #Use the date from the cashflow file
-  date <- seq(as.Date(rates.data[1,1]) %m+% months(1), 
-              as.Date(data[[3]][j]), by="1 months")
+  #date <- seq(as.Date(rates.data[1,1]) %m+% months(1), 
+  #            as.Date(data[[3]][j]), by="1 months")
+  
+  
+  #date.vector <- seq(as.Date(USTData[1,1]), by = 'month', length.out = 600)
+  
   
   spot.rate.curve <- if(method != "dl"){
     spotrates(method = method, 
@@ -568,6 +713,8 @@
                           as.Date(rates.data[1,1]),
                           units = "days")/365
   
+  date.vector <- seq(as.Date(USTData[1,1]), by = 'month', length.out = 600)
+
   disc.curve <- round(exp((spot.rate.curve/yield.basis) * -as.numeric(time.period)),8)
   
   # encapsulate function for forward rate.  Forward.Rate function call the 
@@ -596,8 +743,8 @@
 
   new("TermStructure",
       TradeDate = as.character(rates.data[1,1]),
-      Period = as.numeric(period),
-      Date = as.character(date),
+      Period = seq(1, 600, 1),
+      Date = as.character(date.vector),
       TimePeriod = as.numeric(time.period),
       SpotRate = spot.rate.curve,
       ForwardRate = forward.rate.curve,
